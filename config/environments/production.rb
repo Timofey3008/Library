799d86c32120a6cad1a5c509564ced681e17050e
Timofei <tim148@mail.ru>
2020-09-23 17:23:20 +0300

    Swagger fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => ENV['gmail_username'],
-      :password             => ENV['gmail_password'],
+      :user_name            => "timofeidesyatik@gmail.com",
+      :password             => "34404879Dan",
       :authentication       => "plain",
       :enable_starttls_auto => true
   }
