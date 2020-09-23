e775a4ced7b939b997ae180653322dbca9712a2c
Timofei <tim148@mail.ru>
2020-09-23 15:19:57 +0300

    Swagger fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::API
   private
+
   def error_message
-    {code:401, status:'Unauthorized', data:{message: "Unauthorized access in the API"}}.to_json
+    {code: 401, status: 'Unauthorized', data: {message: "Unauthorized access in the API"}}.to_json
   end
+
   def authenticate
-    authenticate_or_request_with_http_token('Application', error_message ) do |token, options|
+    authenticate_or_request_with_http_token('Application', error_message) do |token, options|
       @user = User.find_by(token: token)
     end
-    # unless @user.present?
-    #   # You could return anything you want if the response if it's unauthorized. in this
-    #   # case I'll just return a json object
-
-    end
   end
+end
 
