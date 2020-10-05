7fa42accf18cf6774ce99092557961ed9cc87bbd
Timofei <tim148@mail.ru>
2020-10-05 19:06:23 +0300

    Moderator email fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,39 +9,39 @@ class UserMailer < ApplicationMailer
   def book_registration
     @user = params[:user]
     @book = params[:book]
-    mail(to: @user.mail, bcc: ENV['moderator_mail'], subject: "#{@book.name} successfully registered")
+    mail(to: @user.mail, bcc: 'tim148@mail.ru', subject: "#{@book.name} successfully registered")
   end
 
   def book_reserved
     @book = params[:book]
     @owner = User.find(@book.owner_id)
     @user = User.find(@book.reader_user_id)
-    mail(to: @user.mail, cc: @owner.mail, bcc: ENV['moderator_mail'], subject: "Book #{@book.name} was successfully reserved")
+    mail(to: @user.mail, cc: @owner.mail, bcc: 'tim148@mail.ru', subject: "Book #{@book.name} was successfully reserved")
   end
 
   def reminder
     @book = params[:book]
     @user = User.find(@book.reader_user_id)
-    mail(to: @user.mail, bcc: ENV['moderator_mail'], subject: 'You need to return book to the Library')
+    mail(to: @user.mail, bcc: 'tim148@mail.ru', subject: 'You need to return book to the Library')
   end
 
   def return
     @book = params[:book]
     @owner = User.find(@book.owner_id)
     @user = User.find(@book.reader_user_id)
-    mail(to: @user.mail, cc: @owner.mail, bcc: ENV['moderator_mail'], subject: "Book #{@book.name} was returned to the Library")
+    mail(to: @user.mail, cc: @owner.mail, bcc: 'tim148@mail.ru', subject: "Book #{@book.name} was returned to the Library")
   end
 
   def return_to_owner
     @book = params[:book]
     @owner = User.find(@book.owner_id)
     @user = User.find(@book.reader_user_id)
-    mail(to: @user.mail, cc: @owner.mail, bcc: ENV['moderator_mail'], subject: "Owner requested return his book #{@book.name}")
+    mail(to: @user.mail, cc: @owner.mail, bcc: 'tim148@mail.ru', subject: "Owner requested return his book #{@book.name}")
   end
 
   def returned
     @book = params[:book]
     @owner = User.find(@book.owner_id)
-    mail(to: @owner.mail, bcc: ENV['moderator_mail'], subject: "Your book #{@book.name} returned")
+    mail(to: @owner.mail, bcc: 'tim148@mail.ru', subject: "Your book #{@book.name} returned")
   end
 end
